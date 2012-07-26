0f003e1d2d948aee697a5b8e638ad8a92fbde063
unknown <robert.klassert@arcor.de>
2012-07-26 11:52:05 +0200

    Changed interface in order to get the time trigger to work.
    Signed-off-by: Atlan1 <robert.klassert@arcor.de>

--- ApiPlus/src/team/ApiPlus/API/Trigger/Trigger.java
+++ ApiPlus/src/team/ApiPlus/API/Trigger/Trigger.java
@@ -1,5 +1,7 @@
 package team.ApiPlus.API.Trigger;
 
+import team.ApiPlus.Util.Task;
+
 /**
  * @author Atlan1
  * @version 1.0
@@ -8,6 +10,6 @@ public interface Trigger {
 
 	public TriggerType getTriggerType();
 	public void setTriggerType(TriggerType tt);
-	public void activate(Object... args);
-	public void triggered(Object... args);
+	public Task activate(Object... args);
+	public Task triggered(Object... args);
 }
